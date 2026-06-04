534f2d083670a3d1264562ffa02a364de56250af
Afaf <afaf20ahadad@gmail.com>
2026-06-04 13:16:48 +0000

    fix: configure routes for dashboard, machines, and maintenances

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,19 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     // Machine management (CRUD without index)
+    Route::get('/machines', [MachineController::class, 'index'])->name('machines.index');
     Route::resource('machines', MachineController::class)->except(['index']);
-
     // Maintenance store
     Route::post('/machines/{machine}/maintenances', [MaintenanceController::class, 'store'])->name('maintenances.store');
+        Route::get('/maintenances', [MaintenanceController::class, 'index'])->name('maintenances.index');
+
 
     // Profile management (from Breeze)
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/maintenances', [MaintenanceController::class, 'index'])->name('maintenances.index');
 
 // Authentication routes (login, register, logout, etc.)
 require __DIR__.'/auth.php';
