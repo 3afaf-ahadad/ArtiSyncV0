eb283a262ba170ce18ecf72487295513139cbc80
Afaf <afaf20ahadad@gmail.com>
2026-06-04 13:16:22 +0000

    feat: add MaintenanceController for intervention tracking

--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Machine;
+use App\Models\Maintenance;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MaintenanceController extends Controller
 {
+    
+    public function index()
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        $query = Maintenance::with('machine.filiere');
+
+        if ($user->isResponsable()) {
+            $query->whereHas('machine', function ($q) use ($user) {
+                $q->where('filiere_id', $user->filiere_id);
+            });
+        }
+
+        $maintenances = $query->orderBy('date', 'desc')->paginate(15);
+
+        return view('maintenance.index', compact('maintenances'));
+    }
+
     public function store(Request $request, Machine $machine)
     {
         /** @var User $user */
